7a72c1f06d5f1b1a5a4ffbc49f360fb700798765
mitali <202112070@daiict.ac.in>
2022-03-19 18:44:17 +0530

    changes done

--- src/pages/Account/Bottom.js
+++ src/pages/Account/Bottom.js
@@ -1,46 +1,40 @@
 import { makeStyles } from "@mui/styles";
 import React from "react";
-import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-
+import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
 
 const useStyles = makeStyles({
-    "logout":{
-        height:"70px",
-        color:"red",
-        backgroundColor:"white",
-        fontWeight:"bold",
-        fontSize:"18px",
-        padding:"10px 10px 10px 10px",
-        bottom: "0px",  
-        position: "relative",
-        display:"flex",
-        borderBottom: "1px solid silver",
-        //marginTop:"50px"
-    }, 
+  logout: {
+    color: "red",
+    backgroundColor: "white",
+    fontWeight: "bold",
+    fontSize: "0.9rem",
+    padding: "10px",
+    bottom: "0px",
+    position: "absolute",
+    display: "flex",
+  },
 
-    "diffIcon":{
-        padding: "10px 30px 17px 30px"
-    }, 
+  diffIcon: {
+    padding: "10px",
+  },
 
-    "menuItems":{
-        paddingTop:"10px"
-    }
+  menuItems: {
+    paddingTop: "10px",
+  },
 });
 
 const Bottom = () => {
-    const classes = useStyles();
-    return(
-        <>
-            <div className={classes["logout"]}>
-                    <div className={classes["diffIcon"]}>
-                        <PowerSettingsNewIcon/>
-                    </div>
-                    <div className={classes["menuItems"]}>
-                        Logout
-                    </div>
-            </div>
-        </>
-    );
-}
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes["logout"]}>
+        <div className={classes["diffIcon"]}>
+          <PowerSettingsNewIcon style={{ fontSize: "0.9rem" }} />
+        </div>
+        <div className={classes["menuItems"]}>Logout</div>
+      </div>
+    </>
+  );
+};
 
 export default Bottom;
